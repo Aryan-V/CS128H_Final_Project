cee8a4cd20296dcf0a6162385b9d7d1c7fcd079a
Edwin Ing <47252149+EdwinIngJ@users.noreply.github.com>
2022-04-08 03:03:58 -0500

    Updated lib

--- src/lib.rs
+++ src/lib.rs
@@ -3,51 +3,70 @@ use reqwest::header::{HeaderMap, AUTHORIZATION};
 use std::env;
 use serde::Deserialize;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Default)]
 pub struct Source {
     id: Option<String>,
     name: String,
 }
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Default)]
 pub struct Article {
     source: Source,
     author: Option<String>,
-    pub title: String,
+    title: String,
     description: String,
     url: String, 
-    urlToImage: Option<String>,
-    publishedAt: String,
+    #[serde(rename="urlToImage")]
+    url_to_image: Option<String>,
+    #[serde(rename="publishedAt")]
+    published_at: String,
     content: String
 }
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Default)]
 pub struct Data {
     status: String,
-    totalResults: i32,
-    pub articles: Vec<Article>
+    #[serde(rename="totalResults")]
+    total_results: i32,
+    articles: Vec<Article>
 }
 
+
+impl Data {
+    //Collects all the article titles into a vector
+    pub fn get_headlines(&self) -> Vec<String> {
+        let mut article_headlines = Vec::new();
+        for article in &self.articles {
+            article_headlines.push(article.title.clone());
+        }
+        article_headlines
+    }
+}
+
+//Helper function to retrieve news articles using an API
 #[tokio::main]
 pub async fn retrieve_news_articles() -> Result<Data, Box<dyn std::error::Error>> {
     dotenv().ok();
-
+    //Retrieve API key from .env file
     let api_key = env::var("NEWS_API_KEY")?;
     
+    //Attaching API key to Authorization HTTP header
     let mut header = HeaderMap::new();
     header.insert(AUTHORIZATION, api_key.parse().unwrap());
 
+    //Sending Request using reqwest
     let client = reqwest::Client::new();
+    
     let response = client
         .get("https://newsapi.org/v2/everything?q=keyword")
         .headers(header)
         .send()
         .await?;
 
-    println!("{:#?}", response);
+    // println!("{:#?}", response);
 
-    //Deserialize reponse into Data objects
+    //Deserialize response into Data objects
     let response_json = response.json::<Data>().await?;
 
-    println!("Response JSON: {:#?}", response_json);
+    // println!("Response JSON: {:#?}", response_json);
 
     Ok(response_json)
 }
