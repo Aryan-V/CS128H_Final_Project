5f934b06cc70b110f887261417b9aba9d913b029
Edwin Ing <47252149+EdwinIngJ@users.noreply.github.com>
2022-04-08 03:04:36 -0500

    Updated main

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,30 @@
-use std::string;
-
+pub mod lib;
+use lib::Data;
 use rust_bert::pipelines::zero_shot_classification::ZeroShotClassificationModel;
-use futures::executor::block_on;
-use lib::Article;
-mod lib;
-
-fn get_titles(to_copy: &Vec<Article>) -> Vec<String> {
-    let mut article_titles = Vec::new();
-    for article in to_copy {
-        article_titles.push(article.title.clone());
-    }
-    article_titles
-}
 
 fn main() {
-    let api_result = lib::retrieve_news_articles();
+    let response = lib::retrieve_news_articles();
 
-    let data = match api_result {
-        Ok(data) => get_titles(&data.articles),
-        _ => Vec::new()
+    let data: Data = match response {
+        Ok(data) => data,
+        _ => panic!("Did not find data")
     };
 
-    // println!("here");
-    for title in &data {
-        print!("{:?}",title);
+    for title in data.get_headlines() {
+        print!("{:?}\n",title);
     }
 
-    let sequence_classification_model = ZeroShotClassificationModel::new(Default::default()).unwrap();
-
-    let input: Vec<&str> = data.iter().map(|s| s.as_ref()).collect();
+    let headlines = data.get_headlines();
+    let input: Vec<&str> = headlines.iter().map(|s| s.as_ref()).collect();
     let candidate_labels = &["angry", "anticipation", "happy", "trust", "fear", "surprise", "sad", "disgust"];
 
+    let sequence_classification_model = ZeroShotClassificationModel::new(Default::default()).unwrap();
     let output = sequence_classification_model.predict(
         input.as_slice(),
         candidate_labels,
         None,
         128,
     );
+
     print!("{:?}",output);
 }
